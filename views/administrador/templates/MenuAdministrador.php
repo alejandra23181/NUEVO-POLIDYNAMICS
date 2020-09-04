89dd8d2a29988a56cba9a00e5ff6baa1bede8e03
Maria Alejandra Montoya A <maria_montoya23181@elpoli.edu.co>
2020-09-04 14:34:17 -0500

    Nuevas plantillas

--- views/administrador/templates/MenuAdministrador.php
+++ views/administrador/templates/MenuAdministrador.php
@@ -9,9 +9,9 @@
       <li><a href="#"> Administración de prestamos</a></li>
       <li><a href="#"> Administración de solicitudes</a></li>
       <li><a href="#"> Administración de usuarios</a></li>
+      <li><a href="#"> Administración de inventario</a></li>
       <li><a href="#"> Auditoria</a></li>
       <li><a href="#"> Reportes</a></li>
-      <li><a href="#"> Configuración</a></li>
       <li><a href="#"> Manual de usuario</a></li>     
       <li><a href="/polidynamics/views/login/Login.php"> Cerrar sesión</a></li>
       
